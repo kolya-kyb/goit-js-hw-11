7aa5d5549315b030f8f7284a19ce54d41d6a18e4
Mykola <107625914+kolya-kyb@users.noreply.github.com>
2022-12-07 15:56:50 +0200

    update

--- src/photoAPI.js
+++ src/photoAPI.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '31824438-78542d665d1f1906f4741f5da';
 
 export async function readPhoto(imageDesc, numberPage, perPage) {
